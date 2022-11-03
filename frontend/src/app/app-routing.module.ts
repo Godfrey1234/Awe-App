c023eb6b2b89a1abc7dba09646fdc9ac42bcc473
SiphoMemane <115017772+SiphoMemane@users.noreply.github.com>
2022-11-03 14:02:52 +0200

    updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
