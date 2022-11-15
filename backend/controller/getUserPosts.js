09253ffcb65684708560423f94fa46c47bcf694f
SiphoMemane <115017772+SiphoMemane@users.noreply.github.com>
2022-11-15 15:16:58 +0200

    getUserPosts

--- backend/controller/getUserPosts.js
+++ backend/controller/getUserPosts.js
@@ -1,19 +1,38 @@
+const { request, response } = require("express");
 const poolConnection = require("../dbConn/dbConn");
 const pool = poolConnection;
 
 //Register fuction 
-const  userPosts = (req, res) => {
+const userPosts = (req, response) => {
 
 
-  //sipho ngwala code
+  //sipho
+  
+
+ 
+
+ 
+    pool.query('SELECT * FROM posts ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+
+
+
+
+
+  }
+
   
 
 
-}
 
 
 
 
 module.exports = {
-    userPosts
+    userPosts,
+
   }  
